6c32aebc234a2f3bbca89580fdb8b93e47776731
BOL <Benjamin.OLeary@d-fine.de>
2023-12-03 17:25:52 +0100

    Add field with Ising-chain domain wall model weightings
    The field objects can be responsible for creating the terms in the
    Hamiltonian which ensure the Ising-chain domain wall model will
    happen.

--- QftBubbleProfileOnQa.py
+++ QftBubbleProfileOnQa.py
@@ -1,6 +1,7 @@
 # from dwave.system import DWaveSampler, EmbeddingComposite
 # import dwave.inspector
 from dimod import ExactSolver
+from hamiltonian.field import Field
 
 # dwave_sampler = DWaveSampler()
 # chosen_sampler = EmbeddingComposite(dwave_sampler)
@@ -8,16 +9,20 @@ chosen_sampler = ExactSolver()
 
 # For the moment, we just use a simple example from the D-Wave Ocean
 # documentation.
-bqm_quadratic_part = \
-    {
-        ('x1', 'x2'): 1,
-        ('x1', 'z'): -2,
-        ('x2', 'z'): -2,
-        ('z', 'z'): 3
-    }
+single_field = \
+    Field(
+        field_name = "t",
+        spatial_point_identifier = "x0",
+        number_of_binary_variables = 4
+    )
+binary_quadratic_model = \
+    single_field.get_weights_for_ICDW(
+        end_spin_weight = 10.0,
+        spin_alignment_weight = 3.5
+    )
 sampling_result = \
     chosen_sampler.sample_qubo(
-        bqm_quadratic_part,
+        binary_quadratic_model,
         # These kwargs are relevant only to the embedded sampler. The exact
         # solver ignores them (after complaining about being given unknown
         # kwargs).
@@ -25,5 +30,17 @@ sampling_result = \
         label = 'SDK Examples - AND Gate'
     )
 
+sample_lowest = sampling_result.lowest(rtol=0.01, atol=0.1)
+print(
+    "[v for v in sample_lowest.variables] = \n",
+    [v for v in sample_lowest.variables]
+)
+print(
+    "bitstrings in above variable order to energies for lowest results = \n",
+    {
+        "".join([f"{x[v]}" for v in sample_lowest.variables]): y
+        for x, y in [(d.sample, d.energy) for d in sample_lowest.data()]
+    }
+)
 # dwave.inspector.show(sample_set)
 print(sampling_result)
