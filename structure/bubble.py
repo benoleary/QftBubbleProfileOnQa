bb5c0fff04d5d936c921c2d93100327e9e582329
BOL <Benjamin.OLeary@d-fine.de>
2023-12-24 17:02:14 +0100

    Add (failing) tests for kinetic term
    This commit adds a stub for getting the weights for the kinetic
    term and tests which should pass once the stub is replaced by the
    correct implementation.

--- structure/bubble.py
+++ structure/bubble.py
@@ -1,7 +1,10 @@
 from typing import Optional
 import minimization.variable
+from minimization.weight import BiasAccumulator
 from configuration.configuration import DiscreteConfiguration
 from hamiltonian.field import FieldAtPoint
+import hamiltonian.potential
+import hamiltonian.kinetic
 
 class ProfileAtPoint:
     """
@@ -24,8 +27,7 @@ class ProfileAtPoint:
                 number_of_values_for_field=number_of_values_for_field,
                 field_step_in_GeV=field_step_in_GeV
             )
-        if second_field_name:
-            self.second_field = FieldAtPoint(
+        self.second_field = None if not second_field_name else FieldAtPoint(
                 field_name=second_field_name,
                 spatial_point_identifier=spatial_point_identifier,
                 number_of_values_for_field=number_of_values_for_field,
@@ -41,6 +43,7 @@ class BubbleProfile:
         """
         The constructor just sets up fields.
         """
+        self.configuration = configuration
         number_of_values_for_field = configuration.number_of_values_for_field
         spatial_name_function = minimization.variable.name_for_index(
             "r",
@@ -58,3 +61,47 @@ class BubbleProfile:
             create_profile_at_point(spatial_index=i)
             for i in range(configuration.number_of_spatial_steps + 1)
         ]
+        self.spin_biases = self._set_up_weights()
+
+    def _set_up_weights(self) -> BiasAccumulator:
+        domain_wall_alignment_weight = (
+            2.0 * self.configuration.maximum_weight_difference
+        )
+        domain_end_weight = 2.0 * domain_wall_alignment_weight
+        calculated_biases = BiasAccumulator()
+        potential_values = (
+            self.configuration.potential_in_quartic_GeV_per_field_step
+        )
+        spatial_step = self.configuration.spatial_step_in_inverse_GeV
+        previous_profile = None
+        for profile_at_point in self.fields_at_points:
+            calculated_biases.add(
+                profile_at_point.first_field.domain_wall_weights(
+                    end_spin_weight=domain_end_weight,
+                    spin_alignment_weight=domain_wall_alignment_weight
+                )
+            )
+            calculated_biases.add(
+                hamiltonian.potential.weights_for(
+                    potential_in_quartic_GeV_per_field_step=potential_values,
+                    single_field=profile_at_point.first_field
+                )
+            )
+            if previous_profile:
+                calculated_biases.add(
+                    hamiltonian.kinetic.weights_for_difference(
+                        at_smaller_radius=previous_profile.first_field,
+                        at_larger_radius=profile_at_point.first_field,
+                        radius_difference_in_inverse_GeV=spatial_step
+                    )
+                )
+                if previous_profile.second_field:
+                    calculated_biases.add(
+                        hamiltonian.kinetic.weights_for_difference(
+                            at_smaller_radius=previous_profile.second_field,
+                            at_larger_radius=profile_at_point.second_field,
+                            radius_difference_in_inverse_GeV=spatial_step
+                        )
+                    )
+            previous_profile = profile_at_point
+        return calculated_biases
--- structure/test_bubble.py
+++ structure/test_bubble.py
@@ -32,5 +32,106 @@ class TestBubbleProfile(unittest.TestCase):
             "incorrect length(s) for spatial identifiers"
         )
 
+    def test_weights_for_monotonic_potential(self):
+        field_step_in_GeV = 1.0
+        test_configuration = DiscreteConfiguration(
+            first_field_name="f",
+            number_of_spatial_steps=1,
+            spatial_step_in_inverse_GeV=1.0,
+            field_step_in_GeV=field_step_in_GeV,
+            potential_in_quartic_GeV_per_field_step=[-1.5, 0.9, 5.3]
+        )
+        test_bubble_profile = BubbleProfile(test_configuration)
+        actual_spin_biases = test_bubble_profile.spin_biases
+
+        # There are 2 points, each with 3 values for the field (so bitstrings
+        # 1000, 1100, and 1110). The maximum potential difference is 6.8, so the
+        # alignment weight should be 13.6 and the end weight should be 27.2.
+        # Each "variable" spin should have a weight equal to half the difference
+        # between its potential and the potential of the previous spin.
+        expected_end_weight = 27.2
+        expected_alignment_weight = -13.6
+
+        # The weight is multiplied by the square of the field step and the
+        # inverse square of the radius step but both are 1.
+        expected_kinetic_weight = 0.125
+
+        lower_index_expected_potential_difference = -1.2
+        upper_index_expected_potential_difference = -2.2
+        expected_linear_biases = {
+            "f_r0_0": expected_end_weight,
+            "f_r0_1": lower_index_expected_potential_difference,
+            "f_r0_2": upper_index_expected_potential_difference,
+            "f_r0_3": -expected_end_weight,
+            "f_r1_0": expected_end_weight,
+            "f_r1_1": lower_index_expected_potential_difference,
+            "f_r1_2": upper_index_expected_potential_difference,
+            "f_r1_3": -expected_end_weight
+        }
+        expected_quadratic_biases = {
+            # There are four pairs which should have only the ICDW alignment
+            # weight.
+            ("f_r0_0", "f_r0_1"): (
+                expected_alignment_weight
+            ),
+            ("f_r0_2", "f_r0_3"): (
+                expected_alignment_weight
+            ),
+            ("f_r1_0", "f_r1_1"): (
+                expected_alignment_weight
+            ),
+            ("f_r1_2", "f_r1_3"): (
+                expected_alignment_weight
+            ),
+            # There are two pairs which have the alignment weight as well as the
+            # kinetic term weight. The rest of the pairs are just from the
+            # kinetic term.
+            ("f_r0_1", "f_r0_1"): expected_kinetic_weight,
+            ("f_r0_1", "f_r0_2"): (
+                expected_alignment_weight + expected_kinetic_weight
+            ),
+            ("f_r0_2", "f_r0_1"): expected_kinetic_weight,
+            ("f_r0_2", "f_r0_2"): expected_kinetic_weight,
+            ("f_r0_1", "f_r1_1"): -expected_kinetic_weight,
+            ("f_r0_1", "f_r1_2"): -expected_kinetic_weight,
+            ("f_r0_2", "f_r1_1"): -expected_kinetic_weight,
+            ("f_r0_2", "f_r1_2"): -expected_kinetic_weight,
+            ("f_r1_1", "f_r0_1"): -expected_kinetic_weight,
+            ("f_r1_1", "f_r0_2"): -expected_kinetic_weight,
+            ("f_r1_2", "f_r0_1"): -expected_kinetic_weight,
+            ("f_r1_2", "f_r0_2"): -expected_kinetic_weight,
+            ("f_r1_1", "f_r1_1"): expected_kinetic_weight,
+            ("f_r1_1", "f_r1_2"): (
+                expected_alignment_weight + expected_kinetic_weight
+            ),
+            ("f_r1_2", "f_r1_1"): expected_kinetic_weight,
+            ("f_r1_2", "f_r1_2"): expected_kinetic_weight
+        }
+
+        self.assertEqual(
+            actual_spin_biases.linear_biases.keys(),
+            expected_linear_biases.keys(),
+            "incorrect linear bias variable names"
+        )
+        for variable_name in actual_spin_biases.linear_biases.keys():
+            self.assertAlmostEqual(
+                expected_linear_biases[variable_name],
+                actual_spin_biases.linear_biases.get(variable_name, 0.0),
+                msg=f"incorrect linear weight for {variable_name}"
+            )
+
+        self.assertEqual(
+            actual_spin_biases.quadratic_biases.keys(),
+            expected_quadratic_biases.keys(),
+            "incorrect quadratic bias variable names"
+        )
+        for variable_name in actual_spin_biases.quadratic_biases.keys():
+            self.assertAlmostEqual(
+                expected_quadratic_biases[variable_name],
+                actual_spin_biases.quadratic_biases.get(variable_name, 0.0),
+                msg=f"incorrect quadratic weight for {variable_name}"
+            )
+
+
 if __name__ == "__main__":
     unittest.main()
