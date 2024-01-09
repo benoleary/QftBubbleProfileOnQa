88d2a77686cdbbdd33dfcf1674bd9b385dacb44d
BOL <Benjamin.OLeary@d-fine.de>
2024-01-09 20:16:49 +0100

    Add getting bubble profiles from CosmoTransitions
    This requires CosmoTransitions to be installed, say by
    
    pip install cosmotransitions
    
    and also that gnuplot is on the path.

---
+++ comparison/cosmotransitions.py
@@ -0,0 +1,44 @@
+from cosmoTransitions import tunneling1D as CTT1
+import potential as P
+
+bubble_profile = CTT1.SingleFieldInstanton(
+    -1.0,
+    1.0,
+    P.ct_potential,
+    P.ct_gradient
+).findProfile()
+
+# print(f"bubble_profile = {bubble_profile}")
+content_for_CSV = (
+    ["r in 1/GeV ; f in GeV"]
+    + [
+        f"{r} ; {f}"
+        for r, f in zip(bubble_profile.R, bubble_profile.Phi)
+    ]
+)
+data_filename = "temporary_gnuplot_input.csv"
+print(f"writing profile in {data_filename}")
+with open(data_filename, "w") as output_file:
+    output_file.write("\n".join(content_for_CSV) + "\n")
+picture_filename = data_filename.rsplit(".", 1)[0] + ".png"
+plotting_filename = "temporary_gnuplot_input.in"
+print(
+    f"running gnuplot on {plotting_filename}"
+)
+with open(plotting_filename, "w") as output_file:
+    output_file.write(
+        f"set title \"CosmoTransitions\"\n"
+        "set datafile separator \";\"\n"
+        "set key autotitle columnhead\n"
+        "unset key\n"
+        "set xlabel \"r in 1/GeV\"\n"
+        "set ylabel \"f in GeV\"\n"
+        "set term png\n"
+        f"set output \"{picture_filename}\"\n"
+        f"plot \"{data_filename}\""
+    )
+import subprocess
+subprocess.call(
+    f"gnuplot {plotting_filename}",
+    shell=True
+)
---
+++ comparison/parameters.py
@@ -0,0 +1,76 @@
+from typing import Callable, Tuple
+
+def for_SM_Higgs(
+        *,
+        linear_factor: float,
+        number_of_steps_from_origin_to_VEV: int,
+        number_of_spatial_steps: int
+    ) -> Tuple[
+        float,
+        float,
+        float,
+        Callable[[int], float],
+        Callable[[float], float],
+        int
+    ]:
+    VEV_in_GeV = 246.0
+    squared_VEV = VEV_in_GeV * VEV_in_GeV
+    quartic_VEV = squared_VEV * squared_VEV
+    mass_of_Higgs_in_GeV = 125.0
+    squared_Higgs_mass = mass_of_Higgs_in_GeV * mass_of_Higgs_in_GeV
+    lambda_coupling = squared_Higgs_mass / (2.0 * squared_VEV)
+    step_factor = 1.0 / number_of_steps_from_origin_to_VEV
+
+    def field_to_GeV(f: int) -> float:
+        return (step_factor * f) - 1.0
+
+    def potential_in_quartic_GeV_from_field_in_GeV(h: float) -> float:
+        return (
+            lambda_coupling
+            * quartic_VEV
+            * ((0.25 * (h**4)) - (0.5 * (h**2)) + (linear_factor * h))
+        )
+
+    return (
+        -VEV_in_GeV,
+        (step_factor * VEV_in_GeV),
+        # This is almost certainly completely wrong.
+        (0.5 / VEV_in_GeV),
+        field_to_GeV,
+        potential_in_quartic_GeV_from_field_in_GeV,
+        (2 * number_of_steps_from_origin_to_VEV) + 1,
+        number_of_spatial_steps
+    )
+
+
+def for_ACS(
+        *,
+        N: int,
+        M: int
+    ) -> Tuple[float, float, float, Callable[[int], float]]:
+    # V = (lambda/8) (phi^2 - a^2)^2 + (epsilon/2a)(phi - a)
+    # a = lambda = 1, epsilon = 0.01
+    # V = (1/8) (phi^2 - 1)^2 + (0.005)(phi - 1)
+    # and phi is normalized to be between -1.0 and +1.0 inclusive.
+
+    def field_to_GeV(f: int) -> float:
+        return ((2.0 * f) / N) - 1.0
+
+    def potential_in_quartic_GeV_from_field_in_GeV(phi: float) -> float:
+        phi_squared_minus_one = (phi * phi) - 1.0
+        return (
+            (0.125 * phi_squared_minus_one * phi_squared_minus_one)
+            + (0.01 * (phi - 1.0))
+        )
+
+    return (
+        -1.0,
+        # The values of phi have to go from -1 to +1.
+        (2.0 / N),
+        # A guess, because they plot rho from 0 to 25 for M = 50.
+        0.5,
+        field_to_GeV,
+        potential_in_quartic_GeV_from_field_in_GeV,
+        N + 1,
+        M
+    )
