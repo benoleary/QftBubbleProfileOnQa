9d19dd6b4fff505cacb6c08862e8555bff4cf27b
BOL <Benjamin.OLeary@d-fine.de>
2024-01-21 20:46:21 +0100

    Fixup

--- hamiltonian/spin.py
+++ hamiltonian/spin.py
@@ -1,7 +1,128 @@
 from typing import List, Optional
 from hamiltonian.hamiltonian import AnnealerHamiltonian
-from hamiltonian.field import FieldDefinition
-from minimization.weight import WeightAccumulator
+from hamiltonian.field import FieldDefinition, FieldAtPoint
+from minimization.weight import WeightAccumulator, WeightTemplate
+
+
+def _weights_for_domain_wall(
+        *,
+        number_of_spins: int,
+        end_spin_weight: float,
+        spin_alignment_weight: float
+) -> WeightTemplate:
+    """
+    This returns the weights to ensure that the spins are valid for the
+    Ising-chain domain wall model, in a form that can be combined with a
+    FieldAtPoint to create a pair of dicts in the form for sample_ising: a
+    dict of linear biases, which could be represented by a vector, and a
+    dict of quadratic biases, which could be represented as an
+    upper-triangular matrix of correlation weights, with zeros on the
+    diagonal. (Apparently it is not necessary that the dict is
+    "upper-triangular"; the middleware seems to cope.)
+    """
+    # This is the case of a FieldAtPoint having correlations between its
+    # own variables.
+    spin_weights = WeightTemplate(
+        first_number_of_values=number_of_spins,
+        second_number_of_values=number_of_spins
+    )
+
+    # First, we set the weights to fix the ends so that there is a domain of
+    # 1s from the first index and a domain of 0s ending at the last index.
+    # The signs are this way because we want the first spin to be |1> which
+    # multiplies its weight by -1 in the objective function, and the last
+    # spin to be |0> which multiplies its weight by +1.
+    spin_weights.first_linear_weights[0] = end_spin_weight
+    spin_weights.first_linear_weights[-1] = -end_spin_weight
+
+    # Next, each pair of nearest neighbors gets weighted to favor having the
+    # same values - which is either (-1)^2 or (+1)^2, so +1, while opposite
+    # values multiply the weighting by (-1) * (+1) = -1. Therefore, a
+    # negative weighting will penalize opposite spins with a positive
+    # contribution to the objective function.
+    for i in range(number_of_spins - 1):
+        spin_weights.quadratic_weights[i][i + 1] = -spin_alignment_weight
+
+    return spin_weights
+
+
+def _weights_for_single_field_potential_at_point(
+        potential_in_quartic_GeV_per_field_step: List[float]
+) -> WeightTemplate:
+    """
+    This function adds calculates weights for spin variables of a single
+    field represented by a set of FieldAtPoint objects, one for every
+    spatial point. (The calculation is significantly different when dealing
+    with multiple fields at a single spatial point.)
+    """
+    # If we have 4 values U_0, U_1, U_2, and U_3, the field has 5 binary
+    # variables, where the first and last are fixed, and there are 4 valid
+    # bitstrings: 10000, 11000, 11100, and 11110. If the middle 3 spin
+    # variables have weights A, B, and C respectively, then we have the
+    # following simultaneous equations when considering the weights brought
+    # to the objective function:
+    # +A+B+C = U_0
+    # -A+B+C = U_1
+    # -A-B+C = U_2
+    # -A-B-C = U_3
+    # Therefore,
+    # U_3 - U_2 = -2 C
+    # U_2 - U_1 = -2 B
+    # U_1 - U_0 = -2 A
+    # and so we end up with the factor 0.5 and the potential for the lower
+    # index minus the potential for the higher index.
+    previous_value = potential_in_quartic_GeV_per_field_step[0]
+    next_values = potential_in_quartic_GeV_per_field_step[1:]
+
+    # This is the case of a single FieldAtPoint with no quadratic weights.
+    spin_weights = WeightTemplate(
+        first_number_of_values=len(next_values),
+        second_number_of_values=0
+    )
+
+    for i, next_value in enumerate(next_values):
+        spin_weights[i] = 0.5 * (previous_value - next_value)
+
+    return spin_weights
+
+
+def _weight_for_ACS_kinetic_term(
+        *,
+        field_step_in_GeV: float,
+        radius_step_in_inverse_GeV: float
+) -> float:
+    """
+    This gives the weight which appears for every combination of spin variables
+    between two FieldAtPoint instances as a positive contribution between those
+    which both belong to the same FieldAtPoint and as a negative contribution
+    between those which belong to different FieldAtPoint instances.
+    """
+    # We assume that the field has the same step size at both spatial
+    # points.
+    return (
+        (0.125 * field_step_in_GeV * field_step_in_GeV)
+        / (radius_step_in_inverse_GeV * radius_step_in_inverse_GeV)
+    )
+
+
+def _constant_quadratics_block(
+        *,
+        field_definition: FieldDefinition,
+        radius_step_in_inverse_GeV: float
+) -> WeightTemplate:
+        number_of_values = field_definition.number_of_values
+        kinetic_constant = _weight_for_ACS_kinetic_term(
+            field_step_in_GeV=field_definition.step_in_GeV,
+            radius_step_in_inverse_GeV=radius_step_in_inverse_GeV
+        )
+        return  WeightTemplate(
+            first_number_of_values=0,
+            second_number_of_values=0,
+            initial_quadratics=[
+                [kinetic_constant for _ in range(number_of_values)]
+                for _ in range(number_of_values)
+            ]
+        )
 
 
 class SpinHamiltonian(AnnealerHamiltonian):
@@ -11,120 +132,73 @@ class SpinHamiltonian(AnnealerHamiltonian):
             first_field: FieldDefinition,
             second_field: Optional[FieldDefinition] = None
     ):
+        self.first_field_definition = first_field
+        self.second_field_definition = second_field
+        self.first_field_domain_wall_template = _weights_for_domain_wall(
+            first_field.number_of_values + 1
+        )
+        self.second_field_domain_wall_template = (
+            None if not second_field
+            else _weights_for_domain_wall(second_field.number_of_values + 1)
+        )
 
-
-
-
-    def weights_for_domain_wall(
+    def domain_wall_weights(
             self,
             *,
-            number_of_spins: int,
-            end_spin_weight: float,
-            spin_alignment_weight: float
+            field_at_point: FieldAtPoint
     ) -> WeightAccumulator:
-        """
-        This returns the weights to ensure that the spins are valid for the
-        Ising-chain domain wall model, in a form that can be combined with a
-        FieldAtPoint to create a pair of dicts in the form for sample_ising: a
-        dict of linear biases, which could be represented by a vector, and a
-        dict of quadratic biases, which could be represented as an
-        upper-triangular matrix of correlation weights, with zeros on the
-        diagonal. (Apparently it is not necessary that the dict is
-        "upper-triangular"; the middleware seems to cope.)
-        """
-        # This is the case of a FieldAtPoint having correlations between its
-        # own variables.
-        spin_weights = WeightAccumulator(
-            first_number_of_values=number_of_spins,
-            second_number_of_values=number_of_spins
+        if field_at_point.field_definition == self.first_field_definition:
+            weight_template = self.first_field_domain_wall_template
+        elif field_at_point.field_definition == self.second_field_definition:
+            weight_template = self.second_field_domain_wall_template
+        else:
+            raise ValueError(
+                f"unknown field {field_at_point.field_definition.field_name}"
+            )
+        return WeightAccumulator(
+            linear_weights=weight_template.first_linears_for_variable_names(
+                field_at_point
+            ),
+            quadratic_weights=weight_template.quadratics_for_variable_names(
+                field_at_point,
+                field_at_point
+            )
         )
 
-        # First, we set the weights to fix the ends so that there is a domain of
-        # 1s from the first index and a domain of 0s ending at the last index.
-        # The signs are this way because we want the first spin to be |1> which
-        # multiplies its weight by -1 in the objective function, and the last
-        # spin to be |0> which multiplies its weight by +1.
-        spin_weights.first_linear_weights[0] = end_spin_weight
-        spin_weights.first_linear_weights[-1] = -end_spin_weight
-
-        # Next, each pair of nearest neighbors gets weighted to favor having the
-        # same values - which is either (-1)^2 or (+1)^2, so +1, while opposite
-        # values multiply the weighting by (-1) * (+1) = -1. Therefore, a
-        # negative weighting will penalize opposite spins with a positive
-        # contribution to the objective function.
-        for i in range(number_of_spins - 1):
-            spin_weights.quadratic_weights[i][i + 1] = -spin_alignment_weight
-
-        return spin_weights
-
-    def weights_for_single_field_potential_at_point(
-            potential_in_quartic_GeV_per_field_step: List[float]
-    ) -> WeightAccumulator:
-        """
-        This function adds calculates weights for spin variables of a single
-        field represented by a set of FieldAtPoint objects, one for every
-        spatial point. (The calculation is significantly different when dealing
-        with multiple fields at a single spatial point.)
-        """
-        # If we have 4 values U_0, U_1, U_2, and U_3, the field has 5 binary
-        # variables, where the first and last are fixed, and there are 4 valid
-        # bitstrings: 10000, 11000, 11100, and 11110. If the middle 3 spin
-        # variables have weights A, B, and C respectively, then we have the
-        # following simultaneous equations when considering the weights brought
-        # to the objective function:
-        # +A+B+C = U_0
-        # -A+B+C = U_1
-        # -A-B+C = U_2
-        # -A-B-C = U_3
-        # Therefore,
-        # U_3 - U_2 = -2 C
-        # U_2 - U_1 = -2 B
-        # U_1 - U_0 = -2 A
-        # and so we end up with the factor 0.5 and the potential for the lower
-        # index minus the potential for the higher index.
-        previous_value = potential_in_quartic_GeV_per_field_step[0]
-        next_values = potential_in_quartic_GeV_per_field_step[1:]
-
-        # This is the case of a single FieldAtPoint with no quadratic weights.
-        spin_weights = WeightAccumulator(
-            first_number_of_values=len(next_values),
-            second_number_of_values=0
-        )
-
-        for i, next_value in enumerate(next_values):
-            spin_weights[i] = 0.5 * (previous_value - next_value)
-
-        return spin_weights
-
-    def weights_for_kinetic_term(
+    def kinetic_weights(
+            self,
             *,
-            number_of_spins: int,
-            field_step_in_GeV: float,
-            radius_step_in_inverse_GeV: float
+            radius_step_in_inverse_GeV: float,
+            nearer_center: FieldAtPoint,
+            nearer_edge: FieldAtPoint,
+            scaling_factor: float
     ) -> WeightAccumulator:
-        """
-        This gives the weights for the part of the kinetic term for a single
-        field over a single step in radius. We could try to arrange things so
-        that each radius has a value for its field(s) and a value for the field
-        difference(s) but in the end it would amount to the same thing while
-        making intermediate steps more complicated.
-        However, we bother only with correlations between spins which can change
-        under the ICDW model, so we give no weight to correlations of the first
-        or last spins of each field.
-        """
-        # We assume that the field has the same step size at both spatial
-        # points.
-        absolute_weight = (
-            (0.125 * field_step_in_GeV * field_step_in_GeV)
-            / (radius_step_in_inverse_GeV * radius_step_in_inverse_GeV)
+        positive_block = _constant_quadratics_block(
+            field_definition=nearer_center.field_definition,
+            radius_step_in_inverse_GeV=radius_step_in_inverse_GeV
         )
+        nearer_center_with_itself = (
+            positive_block.quadratics_for_variable_names(
+                first_field=nearer_center,
+                second_field=nearer_center,
+                scaling_factor=scaling_factor
+            )
+        )
+        nearer_center_with_nearer_edge = (
+            positive_block.quadratics_for_variable_names(
+                first_field=nearer_center,
+                second_field=nearer_edge,
+                scaling_factor=-scaling_factor
+            )
+        )
+        # TODO: set up constant blocks as templates, get dicts, add, return
 
-        for left in (at_smaller_radius, at_larger_radius):
-            for right in (at_smaller_radius, at_larger_radius):
-                signed_weight = (
-                    absolute_weight if (left == right) else -absolute_weight
-                )
-                for l in left.binary_variable_names[1:-1]:
-                    for r in right.binary_variable_names[1:-1]:
-                        weights[(l, r)] = signed_weight
-        return BiasAccumulator(initial_quadratics=weights)
+
+    def potential_weights(
+            self,
+            *,
+            first_field: FieldAtPoint,
+            second_field: Optional[FieldAtPoint] = None
+    ) -> WeightAccumulator:
+        first_field_domain_wall_weights = self.first_field_domain_wall_template
+
--- minimization/weight.py
+++ minimization/weight.py
@@ -10,8 +10,36 @@ def add_to_each(*, in_: List[float], from_: List[float]):
             in_[i] += item_to_add
 
 
-
 class WeightAccumulator:
+    def __init__(
+            self,
+            *,
+            linear_weights: Dict[str, float],
+            quadratic_weights: Dict[Tuple[str, str], float]
+    ):
+        self.linear_weights = linear_weights
+        self.quadratic_weights = quadratic_weights
+
+    def add_linears(self, weights_to_add: Dict[str, float]):
+        for k, v in weights_to_add.items():
+            self.linear_weights[k] = self.linear_weights.get(k, 0.0) + v
+
+    def add_quadratics(self, weights_to_add: Dict[Tuple[str, str], float]):
+        for k, v in weights_to_add.items():
+            self.quadratic_weights[k] = self.quadratic_weights.get(k, 0.0) + v
+
+    def add(self, other: SelfType):
+        self.add_linears(other.linear_weights)
+        self.add_quadratics(other.quadratic_weights)
+
+    def for_QUBO(self) -> Dict[Tuple[str, str], float]:
+        return {
+            **self.quadratic_weights,
+            **{(k, k): v for k, v in self.linear_weights.items()}
+        }
+
+
+class WeightTemplate:
     """
     This class encapsulates the aggregation of weights for linear and quadratic
     biases of spin or bit variables.
@@ -19,7 +47,6 @@ class WeightAccumulator:
     first_linear_weights: List[float]
     second_number_of_values: List[float]
     quadratic_weights: List[List[float]]
-
 
     def __init__(
             self,
@@ -31,16 +58,20 @@ class WeightAccumulator:
             # the same point, but for correlating its own variables with each
             # other.)
             first_number_of_values: int,
-            second_number_of_values: int
+            second_number_of_values: int,
+            initial_quadratics: Optional[List[List[float]]] = None
     ):
         self.first_linear_weights = [0.0 for _ in range(first_number_of_values)]
         self.second_linear_weights = [
             0.0 for _ in range(second_number_of_values)
         ]
-        self.quadratic_weights = [
-            [0.0 for _ in range(first_number_of_values)]
-            for _ in range(second_number_of_values)
-        ]
+        self.quadratic_weights = (
+            initial_quadratics if initial_quadratics
+            else [
+                [0.0 for _ in range(first_number_of_values)]
+                for _ in range(second_number_of_values)
+            ]
+        )
 
     def add_linears_for_first_field(self, weights_to_add: List[float]):
         """
@@ -68,17 +99,6 @@ class WeightAccumulator:
             for j, weight_to_add in enumerate(inner_list):
                 self.quadratic_weights[i][j] += weight_to_add
 
-    def add(self, other: SelfType):
-        """
-        This increments each weight of this WeightAccumulator by its
-        corresponding weight in the other WeightAccumulator, without any check
-        that they were created with the same shape! The other WeightAccumulator
-        may have shorter lists, but must not have longer lists.
-        """
-        self.add_linears_for_first_field(other.first_linear_weights)
-        self.add_linears_for_second_field(other.second_number_of_values)
-        self.add_quadratics(other.quadratic_weights)
-
     def first_linears_for_variable_names(
             self,
             field_at_point: FieldAtPoint
@@ -101,26 +121,11 @@ class WeightAccumulator:
     def quadratics_for_variable_names(
             self,
             first_field: FieldAtPoint,
-            second_field: FieldAtPoint
+            second_field: FieldAtPoint,
+            scaling_factor: float = 1.0
     ) -> Dict[str, float]:
         return {
-            (f, s): self.quadratic_weights[j][i]
+            (f, s): (self.quadratic_weights[j][i] * scaling_factor)
             for i, f in enumerate(first_field.binary_variable_names)
             for j, s in enumerate(second_field.binary_variable_names)
-        }
-
-class WeightDictContainer:
-    def __init__(
-            self,
-            *,
-            linear_weights: Dict[str, float],
-            quadratic_weights: Dict[Tuple[str, str], float]
-    ):
-        self.linear_weights = linear_weights
-        self.quadratic_weights = quadratic_weights
-
-    def for_QUBO(self) -> Dict[Tuple[str, str], float]:
-        return {
-            **self.quadratic_weights,
-            **{(k, k): v for k, v in self.linear_weights.items()}
         }
