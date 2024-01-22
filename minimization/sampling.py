07abb6b4627b679a1af4befe3a099624360077b7
BOL <Benjamin.OLeary@d-fine.de>
2024-01-22 23:00:29 +0100

    Fixup

--- minimization/sampling.py
+++ minimization/sampling.py
@@ -24,23 +24,16 @@ def get_sample(
         sampler_name: str
 ) -> SampleSet:
     chosen_sampler = get_sampler(sampler_name)
+    appropriate_arguments = {
+            "h": spin_biases.linear_biases,
+            "J": spin_biases.quadratic_biases
+    }
     if message_for_Leap and (sampler_name == "dwave"):
-        return chosen_sampler.sample_ising(
-            h=spin_biases.linear_biases,
-            J=spin_biases.quadratic_biases,
-            num_reads=number_of_shots,
-            label=message_for_Leap
-        )
+        appropriate_arguments["num_reads"] = number_of_shots
+        appropriate_arguments["label"] = message_for_Leap
     if "kerberos" == sampler_name:
-        return chosen_sampler.sample_ising(
-            h=spin_biases.linear_biases,
-            J=spin_biases.quadratic_biases,
-            max_iter=10
-        )
-    return chosen_sampler.sample_ising(
-        h=spin_biases.linear_biases,
-        J=spin_biases.quadratic_biases
-    )
+        appropriate_arguments["max_iter"] = 10
+    return chosen_sampler.sample_ising(**appropriate_arguments)
 
 def get_lowest_sample_from_set(sample_set: SampleSet) -> Dict[str, float]:
     lowest_energy_sample, = next(
